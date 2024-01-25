ef27770dbfa47094774a277a30d2e10566b80cb6
Shaktidhar <sdmohapatra7@gmail.com>
2024-01-25 20:41:46 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pb-5 pt-3 bg-dark">
+      <h2 className="text-warning text-center mt-4 mb-5">Expression Form</h2>
     </div>
   );
 }
